4a2f0ad5e2c5d37c36976c083bcb2d6ce76bb458
LavanyaKanu <lavanya.kanukula@gmail.com>
2023-09-28 15:23:26 -0300

    Added Test Cases

--- src/main/java/com/information/customer/model/Customer.java
+++ src/main/java/com/information/customer/model/Customer.java
@@ -1,15 +1,15 @@
 package com.information.customer.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
-import java.sql.Date;
-
+@Data
 @Entity
 @Table(name = "Customers")
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "customerid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id"/*, nullable = true, columnDefinition="tinyint(1) default 1"*/)
     private Long customerId;
     @Column(name = "first_name", nullable = false, length = 50)
     private String firstName;
@@ -17,69 +17,38 @@ public class Customer {
     private String lastName;
     @Temporal(TemporalType.DATE)
     @Column(name = "date_of_birth")
-    private Date dateOfBirth;
+    private String dateOfBirth;
     @Column(name = "gender")
     private String gender;
     @Column(name = "phone_number", length = 20)
     private String phoneNumber;
 
-    public Customer(){
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    /*@ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
+            @JoinTable(name = "user_policy_info",
+            joinColumns = @JoinColumn(name = "customer_id"*//*, nullable = true, columnDefinition="tinyint(1) default 1"*//*),
+            inverseJoinColumns = @JoinColumn(name = "policy_id"))
+    private List<Policy> policies;*/
+    /*@ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
+    @JoinTable(name = "user_policy",
+            joinColumns = @JoinColumn(name = "customer_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id") )
+    private List<UserPolicy> user_policies;*/
+    /*@OneToOne(fetch=FetchType.EAGER, optional=true, cascade=CascadeType.ALL)
+    private UserPolicy user_policy;*/
+    public Customer() {
 
     }
-    public Customer(Long customerId, String firstName, String lastName, Date dateOfBirth, String gender, String phoneNumber) {
-        this.customerId = customerId;
+
+    public Customer(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, Address address) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
-    }
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerID(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.address = address;
     }
 }

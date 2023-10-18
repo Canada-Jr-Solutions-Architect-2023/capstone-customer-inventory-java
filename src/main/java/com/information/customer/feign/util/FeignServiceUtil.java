8b75c2a2e66425fd30a2d320fee6ddca70ec13f4
LavanyaKanu <lavanya.kanukula@gmail.com>
2023-10-18 14:20:06 -0300

    integrating payments microservice

--- src/main/java/com/information/customer/feign/util/FeignServiceUtil.java
+++ src/main/java/com/information/customer/feign/util/FeignServiceUtil.java
@@ -1,11 +1,17 @@
 package com.information.customer.feign.util;
 
+import com.information.customer.model.Policy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(name="feign", url="https://carehub-quotes.ee-cognizantacademy.com/api")
 public interface FeignServiceUtil {
 
     @GetMapping("/policy/feignTest")
     public String getDemo();
+
+    @GetMapping("/policy/all")
+    public List<Policy> getAllPolicies();
 }
